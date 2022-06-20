d40e01cb743921f8579bcc66246f6e336080ab42
Steven Sun <gsuuon+dev@gmail.com>
2022-06-20 15:25:39 -0500

    Object Expression interface dependency injection

---
+++ FSUI/src/Design/Scratch.ObjectExpression.fs
@@ -0,0 +1,90 @@
+namespace Scratch.ObjectExpression
+
+open Design.Common
+
+module CommonNodeInterfaces =
+    type IElement<'Data, 'Visual> =
+        abstract member Create : 'Data -> 'Visual
+
+    type IText<'Visual> =
+        abstract member Text : IElement<string, 'Visual>
+
+    type IContainer<'Visual, 'Container> =
+        abstract member Container : IElement<List<'Visual>, 'Container>
+
+module ImplementationHost =
+    open CommonNodeInterfaces
+    open Design.Host
+
+    type Env() =
+        interface IText<IVisualElement> with
+            member val Text =
+                { new IElement<string, _> with
+                    member _.Create content = new VisualTextElement (content) :> IVisualElement
+                }
+
+        interface IContainer<IVisualElement, VisualContainer> with
+            member val Container =
+                { new IElement<List<IVisualElement>, VisualContainer> with
+                    member _.Create children =
+                        new VisualContainer (children)
+                }
+
+module RenderElements =
+    open CommonNodeInterfaces
+
+    let text content =
+        fun (env: #IText<_>) (pos: Position) ->
+            env.Text.Create content
+
+    let div (children: List<'env -> Position -> 'visual>) =
+        fun (env: #IContainer<_,_>) (pos: Position) ->
+            let x =
+                children
+                 |> List.map
+                    (fun x -> x env pos )
+            let y = x |> env.Container.Create
+            ()
+
+module View =
+    open RenderElements
+    open ImplementationHost
+
+    let view : Env -> Position -> Unit =
+        div [
+            text "hi"
+        ]
+
+    view (Env()) (Ordinal 0)
+
+    // TODO-next how does the cache work then?
+
+
+module ScratchInterfaceType =
+    type Foo = interface end
+    type FooBar() = interface Foo
+
+    let foo (fn: int -> #Foo) = fn 0
+    
+    let foobar (x: int) = FooBar()
+
+    let x = foo foobar
+
+module ScratchInterfaceTypeDetailed =
+    type IVisEl = interface end
+    type VisText(c: string) =
+        interface IVisEl
+    type VisNum(i: int) =
+        interface IVisEl
+
+    let div (children: List<int -> #IVisEl>) =
+        children |> List.mapi (fun idx x -> x idx)
+
+    let text (content: string) (idx: int) = VisText content :> IVisEl
+    let num (c: int) (idx: int) = VisNum c :> IVisEl
+
+    let view =
+        div [
+            text "hi"
+            num 0
+        ]
