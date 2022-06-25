488477a0a1dee819ae089fb3ce35219a0f76b1e8
Steven Sun <gsuuon+dev@gmail.com>
2022-06-25 13:21:32 -0500

    Test cache swap

--- FSUI/tests/Design.UsageSketch.Test/Program.fs
+++ FSUI/tests/Design.UsageSketch.Test/Program.fs
@@ -1,11 +1,42 @@
 open Design.UsageSketch
 
 module Program =
-
-    [<EntryPoint>]
-    let main _ =
+    let testCacheSwap () =
         Component.renderView "world"
         printfn "%A" Component.freshStale
         Component.renderView "toy world"
         printfn "%A" Component.freshStale
+
+    module DerivedStaticGeneric =
+        open Scratch.DerivedStaticGeneric
+
+        type Foo() = class end
+        type Bar = {
+            x : int
+        }
+
+        let testDerivedStaticGeneric () =
+            Cache.CacheA.Set 0 (Foo())
+            Cache.CacheA.Set 1 ({x = 0})
+            let swapper = Cache.DefaultSwapper()
+
+            printfn "Foo 0 %A" <| swapper.Retrieve<Foo> 0
+            printfn "Foo 1 %A" <| swapper.Retrieve<Foo> 1
+            printfn "Bar 0 %A" <| swapper.Retrieve<Bar> 0
+            printfn "Bar 1 %A" <| swapper.Retrieve<Bar> 1
+            
+        type MyCacheA<'T>() = inherit BaseStatic<'T>()
+        type MyCacheB<'T>() = inherit BaseStatic<'T>()
+
+        type MySwapper() =
+            let isA = false
+            member _.Ret
+
+        let testDerivedStaticCustomCache () =
+            ()
+        
+
+    [<EntryPoint>]
+    let main _ =
+        testDerivedStaticGeneric()
         0
