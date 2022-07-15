21c8e0d5e3c56d8b69a6ef35c7f0262d9b3669c2
Steven Sun <gsuuon+dev@gmail.com>
2022-07-15 13:44:37 -0500

    Props first (should probably work this change up)

--- FSUI/src/FSUI/Elements/Views.fs
+++ FSUI/src/FSUI/Elements/Views.fs
@@ -3,7 +3,7 @@ module FSUI.Elements.Views
 open FSUI.Elements.Interfaces
 open FSUI.Renderer.Element
 
-let inline render ([<InlineIfLambda>]readElement: 'env -> Element<_,_,_,_>) data props env pos =
+let inline render ([<InlineIfLambda>]readElement: 'env -> Element<_,_,_,_>) props data env pos =
     (readElement env).Render data props pos
 
 let inline text x = render (fun (env:#IText<_,_,_>) -> env.Text) x
