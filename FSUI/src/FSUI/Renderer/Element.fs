4184d4b0c4e480991d9162f095c587fb86c4e29b
Steven Sun <gsuuon+dev@gmail.com>
2022-07-19 15:57:34 -0500

    Pass cache to element directly

--- FSUI/src/FSUI/Renderer/Element.fs
+++ FSUI/src/FSUI/Renderer/Element.fs
@@ -1,6 +1,7 @@
 namespace FSUI.Renderer.Element
 
 open FSUI.Renderer.Cache
+open FSUI.Renderer.Provider
 
 type Position =
     | Ordinal of parent: Position * int
@@ -12,23 +13,20 @@ type Element<'data, 'props, 'visual, 'node>
     (
         [<InlineIfLambda>]
         wrap: 'visual -> 'node,
-        [<InlineIfLambda>]
-        remove: 'visual -> unit,
-        swappers: Swappers
+        cache: Swapper<Position,'data,'props,'visual>
     ) =
-    abstract member Create: 'data -> 'props -> 'visual
-    abstract member Update: 'data -> 'props -> 'visual -> 'data -> 'props -> 'visual
-
-    member val Cache = swappers.Create(remove)
-
-    member inline private this.Save pos data props el =
-        this.Cache.Fresh.Add(pos, (data, props, el))
+    let save pos data props el =
+        cache.Fresh.Add(pos, (data, props, el))
         el
 
     member this.Render (data: 'data) (props: 'props) (pos: Position) : 'node =
-        match this.Cache.Stale.TryGetValue pos with
+        match cache.Stale.TryGetValue pos with
         | true, (cachedData, cachedProps, cachedVisual) ->
             this.Update cachedData cachedProps cachedVisual data props
-        | _ -> this.Create data props
-        |> this.Save pos data props
+        | _ ->
+            this.Create data props
+        |> save pos data props
         |> wrap
+
+    abstract member Create: 'data -> 'props -> 'visual
+    abstract member Update: 'data -> 'props -> 'visual -> 'data -> 'props -> 'visual
