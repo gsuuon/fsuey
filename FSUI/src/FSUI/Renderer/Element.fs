68d12a72f1ecea57bfb5647daa0fc7bf8315ccc9
Steven Sun <gsuuon+dev@gmail.com>
2022-08-05 13:24:16 -0500

    Separate IElement from Element

--- FSUI/src/FSUI/Renderer/Element.fs
+++ FSUI/src/FSUI/Renderer/Element.fs
@@ -1,4 +1,4 @@
-namespace FSUI.Renderer.Element
+module FSUI.Renderer.Element
 
 open FSUI.Renderer.Cache
 open FSUI.Renderer.Provider
@@ -8,35 +8,39 @@ type Position =
     | Nominal of parent: Position * name: string * insertAfter: int
     | Root
 
-/// Semantics determined by application
-/// 'data is the input type
-/// 'props is differenced items, properties. TODO
-/// 'visual is the cached type. Typically the actual item rendered, so that update is type-safe.
-/// 'node is the collection type to be used in containers. Typically a root type.
-///    'node is not cached, but is re-wrapped each render to provide to container.
-///     Generally best to be statically castable.
-[<AbstractClass>]
-type Element<'data, 'props, 'visual, 'node>
-    (
-        [<InlineIfLambda>]
-        wrap: 'visual -> 'node,
-        cache: Swapper<Position,'data,'props,'visual>
-    ) =
-    let save pos data props el =
-        cache.Fresh.Add(pos, (data, props, el))
-        el
-
-    member this.Render (data: 'data) (props: 'props) (pos: Position) : 'node =
-        let (exists, stale) = cache.Stale.Remove pos
-            // for some reason normal match with syntax doesn't work here
-        
-        match exists, stale with
-        | true, (cachedData, cachedProps, cachedVisual) ->
-            this.Update cachedData cachedProps cachedVisual data props
-        | _ ->
-            this.Create data props
-        |> save pos data props
-        |> wrap
-
+type IElement<'data, 'props, 'visual> =
     abstract member Create: 'data -> 'props -> 'visual
     abstract member Update: 'data -> 'props -> 'visual -> 'data -> 'props -> 'visual
+
+type ElementRecord<'d, 'p, 'v> =
+    { create : 'd -> 'p -> 'v
+      update : 'd -> 'p -> 'v -> 'd -> 'p -> 'v
+    }
+    interface IElement<'d, 'p, 'v> with
+        member this.Create a b = this.create a b
+        member this.Update a b c d e = this.update a b c d e
+
+let create<'data, 'props, 'visual, 'node, 'element when 'element :> IElement<'data, 'props, 'visual>>
+    (wrap: 'visual -> 'node)
+    (cache: Swapper<Position, 'data, 'props, 'visual>)
+    (element: 'element)
+        : 'data -> 'props -> Position -> 'node
+    =
+    fun (data: 'data) (props: 'props) (pos: Position) ->
+        let (exists, (data', props', visual')) = cache.Stale.Remove pos // match .. with syntax doesn't work to get the correct overload
+        
+        let visual =
+            if exists then
+                element.Update data' props' visual' data props
+            else
+                element.Create data props
+
+        visual
+         |> fun el ->
+                try
+                    cache.Fresh.Add (pos, (data, props, el) )
+                with
+                | :? System.ArgumentException -> // TODO Do we care about this?
+                    System.Console.Error.WriteLine ("Fresh cache already contained an element at " + pos.ToString())
+                el
+         |> wrap
