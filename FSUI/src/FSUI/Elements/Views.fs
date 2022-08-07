e487bdf7c2166442f301b47a5f971ba8fb5e1f46
Steven Sun <gsuuon+dev@gmail.com>
2022-08-07 05:25:58 -0500

    Props first stragglers

--- FSUI/src/FSUI/Elements/Views.fs
+++ FSUI/src/FSUI/Elements/Views.fs
@@ -3,32 +3,32 @@ module FSUI.Elements.Views
 open FSUI.Elements.Interfaces
 open FSUI.Renderer.Element
 
-let inline render<'data, 'props, 'visual, 'node, 'env>
+let inline mkRender<'props, 'data, 'node, 'env>
     ([<InlineIfLambda>]
-        renderer: 'env -> RendersElement<'data,'props,'visual>)
+        renderer: 'env -> RendersElement<'props, 'data, 'node>)
     (props: 'props)
     (data: 'data)
     (env: 'env)
     (pos: Position)
     =
-    (renderer env) data props pos
+    (renderer env) props data pos
 
-let text x = render (fun (env: #IText<_,_>) -> env.Text) x
-let gameObject x = render (fun (env: #IGameObject<_,_,_>) -> env.GameObject) x
+let text x = mkRender (fun (env: #IText<'props, 'node>) -> env.Text) x
+let gameObject x = mkRender (fun (env: #IGameObject<_,_,_>) -> env.GameObject) x
 
 let div props children (env: #IContainer<_,_>) pos =
     let nodes : 'node list =
         children |> List.mapi (fun idx fnode -> fnode env (Ordinal (pos, idx)))
 
-    env.Container nodes props pos
+    env.Container props nodes pos
 
 let button props (child: 'env -> Position -> 'node, action: unit -> unit) (env: #IButton<_,_,_>) pos =
     let childNode = child env (Ordinal (pos, 0))
 
-    env.Button (childNode, action) props pos
+    env.Button props (childNode, action) pos
 
 let join props children (env: #IJoinContain<_,_,_>) pos =
     let nodes : 'node list =
         children |> List.mapi (fun idx fnode -> fnode env (Ordinal (pos, idx)))
 
-    env.JoinContain nodes props pos
+    env.JoinContain props nodes pos
