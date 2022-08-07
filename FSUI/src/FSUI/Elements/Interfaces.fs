d8e5780c4ea1e495c81f5801d91cb3f9e6c39d54
Steven Sun <gsuuon+dev@gmail.com>
2022-08-07 02:48:41 -0500

    Just kidding they should be RendersElement

--- FSUI/src/FSUI/Elements/Interfaces.fs
+++ FSUI/src/FSUI/Elements/Interfaces.fs
@@ -3,21 +3,21 @@ namespace FSUI.Elements.Interfaces
 open FSUI.Renderer.Element
 
 type IPoly<'data, 'props, 'visual> =
-    abstract member Poly : IElement<'data, 'props, 'visual>
+    abstract member Poly : RendersElement<'data, 'props, 'visual>
 
 type IContainer<'data, 'props, 'node> =
-    abstract member Container: IElement<'node list, 'props, 'node>
+    abstract member Container: RendersElement<'node list, 'props, 'node>
 
 type IText<'props, 'visual> =
-    abstract member Text: IElement<string, 'props, 'visual>
+    abstract member Text: RendersElement<string, 'props, 'visual>
 
 type IButton<'data, 'props, 'visual> =
-    abstract member Button: IElement<'data, 'props, 'visual>
+    abstract member Button: RendersElement<'data, 'props, 'visual>
 
 type IGameObject<'data, 'props, 'visual> =
-    abstract member GameObject : IElement<'data, 'props, 'visual>
+    abstract member GameObject : RendersElement<'data, 'props, 'visual>
 
 /// Let a type from one type hierarchy to join/contain children of another type hierarchy
 /// e.g. a VisualElement containing GameObject
 type IJoinContain<'child, 'props, 'container> =
-    abstract member JoinContain : IElement<'child, 'props, 'container>
+    abstract member JoinContain : RendersElement<'child, 'props, 'container>
