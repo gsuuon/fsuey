73af409c2a6c33a0e6c980f207220ce99dae1276
Steven Sun <gsuuon+dev@gmail.com>
2022-08-07 03:03:45 -0500

    Update hierarchy operations

--- FSUI/src/FSUI.Renderer.Unity/Hierarchy.fs
+++ FSUI/src/FSUI.Renderer.Unity/Hierarchy.fs
@@ -3,47 +3,20 @@ namespace FSUI.Renderer.Unity.Hierarchy
 open UnityEngine
 open UnityEngine.UIElements
 
-type GameObjectNode =
-    { gameObject : GameObject
-      mutable visualElementParent : VisualElement
-    }
-
 type UnityNode =
-    | Screen of VisualElement
-    | World of GameObjectNode
-        // TODO probably not actually a game object, just too tired rn
-        // don't think we want to just instantiate game objects here
-        // load resource?
-        // prefab path?
+    static member addChild (parent: VisualElement) =
+        fun (child: VisualElement) -> parent.Add child
 
-    static member addChild (child: UnityNode, parent: GameObjectNode) =
-        match child, parent with
-        | Screen screenChild, { visualElementParent = visualParent } ->
-            visualParent.Add screenChild
-
-        | World gobjChild, _ -> 
-            gobjChild.gameObject.transform.SetParent parent.gameObject.transform
-
-    static member addChild (child: UnityNode, parent: VisualElement) =
-        match child with
-        | Screen screenChild ->
-            parent.Add screenChild
-        | World gNode -> 
-            gNode.visualElementParent <- parent
+    static member addChild (parent: GameObject) =
+        fun (child: GameObject) -> child.transform.SetParent parent.transform
 
     static member addChildren(children: VisualElement list, parent: VisualElement) =
-        children |> List.iter parent.Add
+        children |> List.iter (UnityNode.addChild parent)
         parent
 
-    static member addChildren(children: UnityNode list, parent: VisualElement) =
-        children
-         |> List.iter
-            (function
-             | Screen child -> parent.Add child
-             | World x -> () // TODO
-             )
-
+    static member addChildren(children: GameObject list, parent: GameObject) =
+        children |> List.iter (UnityNode.addChild parent)
         parent
 
     static member inline remove (x: #VisualElement) = x.RemoveFromHierarchy()
-    static member inline remove (x: GameObjectNode) = GameObject.Destroy x.gameObject
+    static member inline remove (x: GameObject) = GameObject.Destroy x
