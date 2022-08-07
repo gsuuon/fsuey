4dda00da7d037c565e7307ce1b5f697ed7305c4f
Steven Sun <gsuuon+dev@gmail.com>
2022-08-07 03:56:14 -0500

    Refactor Graph operations

--- FSUI/src/FSUI.Renderer.Unity/Graph.fs
+++ FSUI/src/FSUI.Renderer.Unity/Graph.fs
@@ -3,20 +3,22 @@ namespace FSUI.Renderer.Unity
 open UnityEngine
 open UnityEngine.UIElements
 
-type Graph =
-    static member addChild (parent: VisualElement) =
-        fun (child: VisualElement) -> parent.Add child
-
-    static member addChild (parent: GameObject) =
-        fun (child: GameObject) -> child.transform.SetParent parent.transform
-
-    static member addChildren(children: VisualElement list, parent: VisualElement) =
-        children |> List.iter (Graph.addChild parent)
+module ScreenElement =
+    let addChild (parent: VisualElement) (child: VisualElement) =
+        parent.Add child
         parent
+
+module WorldElement =
+    let addChild (parent: GameObject) (child: GameObject) =
+        child.transform.SetParent parent.transform
+        parent
+
+type Graph =
+    static member addChildren(children: VisualElement list, parent: VisualElement) =
+        children |> List.fold ScreenElement.addChild parent
 
     static member addChildren(children: GameObject list, parent: GameObject) =
-        children |> List.iter (Graph.addChild parent)
-        parent
+        children |> List.fold WorldElement.addChild parent
 
     static member inline remove (x: #VisualElement) = x.RemoveFromHierarchy()
     static member inline remove (x: GameObject) = GameObject.Destroy x
