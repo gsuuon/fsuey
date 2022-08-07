7de929e2e098af4be04ee3e3039558e9d3edb9dc
Steven Sun <gsuuon+dev@gmail.com>
2022-08-07 02:52:07 -0500

    Views use new interfaces

--- FSUI/src/FSUI/Elements/Views.fs
+++ FSUI/src/FSUI/Elements/Views.fs
@@ -5,31 +5,30 @@ open FSUI.Renderer.Element
 
 let inline render<'data, 'props, 'visual, 'node, 'env>
     ([<InlineIfLambda>]
-        readElement: 'env -> Element<'data,'props,'visual,'node>)
+        renderer: 'env -> RendersElement<'data,'props,'visual>)
     (props: 'props)
     (data: 'data)
     (env: 'env)
     (pos: Position)
     =
-    (readElement env).Render data props pos
+    (renderer env) data props pos
 
-let text x = render (fun (env:#IText<_,_,_>) -> env.Text) x
-let image x = render (fun (env: #IImage<_,_,_>) -> env.Image) x
-let gameObject x = render (fun (env: #IGameObject<_,_,_,_>) -> env.GameObject) x
+let text x = render (fun (env: #IText<_,_>) -> env.Text) x
+let gameObject x = render (fun (env: #IGameObject<_,_,_>) -> env.GameObject) x
 
 let div props children (env: #IContainer<_,_,_>) pos =
     let nodes : 'node list =
         children |> List.mapi (fun idx fnode -> fnode env (Ordinal (pos, idx)))
 
-    env.Container.Render nodes props pos
+    env.Container nodes props pos
 
-let button props (child: 'env -> Position -> 'node, action: unit -> unit) (env: #IButton<_,_,_,_>) pos =
+let button props (child: 'env -> Position -> 'node, action: unit -> unit) (env: #IButton<_,_,_>) pos =
     let childNode = child env (Ordinal (pos, 0))
 
-    env.Button.Render (childNode, action) props pos
+    env.Button (childNode, action) props pos
 
-let join props children (env: #IJoinContain<_,_,_,_>) pos =
+let join props children (env: #IJoinContain<_,_,_>) pos =
     let nodes : 'node list =
         children |> List.mapi (fun idx fnode -> fnode env (Ordinal (pos, idx)))
 
-    env.JoinContain.Render nodes props pos
+    env.JoinContain nodes props pos
