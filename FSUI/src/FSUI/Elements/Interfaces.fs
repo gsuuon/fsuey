cba0789447acea9a25ef1e790baa88fa7d3fac5c
Steven Sun <gsuuon+dev@gmail.com>
2022-08-07 02:38:29 -0500

    Interfaces use IElement

--- FSUI/src/FSUI/Elements/Interfaces.fs
+++ FSUI/src/FSUI/Elements/Interfaces.fs
@@ -2,25 +2,22 @@ namespace FSUI.Elements.Interfaces
 
 open FSUI.Renderer.Element
 
-type IElement<'data, 'props, 'visual, 'node> =
-    abstract member Element : Element<'data, 'props, 'visual, 'node>
+type IPoly<'data, 'props, 'visual> =
+    abstract member Poly : IElement<'data, 'props, 'visual>
 
-type IContainer<'props, 'visual, 'node> =
-    abstract member Container: Element<'node list, 'props, 'visual, 'node>
+type IContainer<'data, 'props, 'node> =
+    abstract member Container: IElement<'node list, 'props, 'node>
 
-type IText<'props, 'visual, 'node> =
-    abstract member Text: Element<string, 'props, 'visual, 'node>
+type IText<'props, 'visual> =
+    abstract member Text: IElement<string, 'props, 'visual>
 
-type IImage<'props, 'visual, 'node> =
-    abstract member Image: Element<string, 'props, 'visual, 'node>
+type IButton<'data, 'props, 'visual> =
+    abstract member Button: IElement<'data, 'props, 'visual>
 
-type IButton<'data, 'props, 'visual, 'node> =
-    abstract member Button: Element<'data, 'props, 'visual, 'node>
-
-type IGameObject<'data, 'props, 'visual , 'node> =
-    abstract member GameObject : Element<'data, 'props, 'visual, 'node>
+type IGameObject<'data, 'props, 'visual> =
+    abstract member GameObject : IElement<'data, 'props, 'visual>
 
 /// Let a type from one type hierarchy to join/contain children of another type hierarchy
 /// e.g. a VisualElement containing GameObject
-type IJoinContain<'data, 'props, 'visual, 'childType> =
-    abstract member JoinContain : Element<'data, 'props, 'visual, 'childType>
+type IJoinContain<'child, 'props, 'container> =
+    abstract member JoinContain : IElement<'child, 'props, 'container>
