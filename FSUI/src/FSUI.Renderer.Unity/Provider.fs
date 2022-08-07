6c263fbf7b492d2483f25224f2689256303d34d1
Steven Sun <gsuuon+dev@gmail.com>
2022-08-06 21:07:03 -0500

    Sanity check new interface

--- FSUI/src/FSUI.Renderer.Unity/Provider.fs
+++ FSUI/src/FSUI.Renderer.Unity/Provider.fs
@@ -85,7 +85,7 @@ module Element =
             member this.Create a b = this.create a b
             member this.Update a b c d e = this.update a b c d e
 
-    let create wrap (cache: Swapper<_,_,_,_>) (element: #IElement<_,_,_>) =
+    let create (wrap: 'visual -> 'node) (cache: Swapper<_,_,_,_>) (element: #IElement<_,_,_>) =
         fun (data: 'data) (props: 'props) (pos: Position) ->
             let (exists, stale) = cache.Stale.Remove pos
                 // for some reason normal match with syntax doesn't work here
@@ -98,22 +98,68 @@ module Element =
             |> fun el -> cache.Fresh.Add(pos, (data, props, el)); el
             |> wrap
 
-// module Foo =
-//     type Provider () =
-//         let screenSpace x = element base.mkCache x
-//         interface IText<..> with
-//             member val Text =
-//                 screenSpace {
-//                     create = ..
-//                     update = ..
-//                 }
+    type RenderElement<'data, 'props, 'node> = 'data -> 'props -> Position -> 'node
 
+    module Interfaces =
+        type IText<'props, 'visual, 'node> =
+            abstract Text : RenderElement<string, 'props, 'node>
 
-//     module Views =
-//         let text props data =
-//             fun (env: #IText) pos ->
-//                 env.Text props data pos
+        type IContainer<'props, 'node> =
+            abstract Container : RenderElement<List<'node>, 'props, 'node>
 
+    module SanityCheck =
+        open Interfaces
+
+        module Views =
+            let text props data =
+                fun (env: #IText<_,_,_>) pos ->
+                    env.Text data props pos
+
+            let div props children =
+                fun (env: #IContainer<'props, 'nodes>) pos ->
+                    let childNodes : List<'nodes> =
+                        children
+                         |> List.mapi
+                            ( fun idx render -> render env (Ordinal (pos, idx)) )
+
+                    env.Container childNodes props pos
+
+        module Layout =
+            open Views
+
+            let layout () =
+                div [] [
+                    text [] "hey"
+                ]
+
+        type Provider () =
+            let swappers = Swappers()
+            let screen x = create (fun (x: #VisualElement) -> x :> VisualElement) (swappers.Create UnityNode.remove) x
+
+            interface IText<UnityProps, Label, VisualElement> with
+                member val Text =
+                    screen {
+                        create = fun d p -> Label d
+                        update = fun d' p' el d p -> el.text <- d; el
+                    }
+
+            interface IContainer<UnityProps, VisualElement> with
+                member val Container =
+                    screen {
+                        create = fun d p ->
+                            let el = VisualElement()
+                            for child in d do
+                                el.Add child
+                            el
+                        update = fun d' p' el d p -> el // so on
+                    }
+
+        module App =
+            open FSUI.Renderer.Element
+
+            let env = Provider()
+            let view () =
+                (Layout.layout ()) env Root
 
 open FSUI.Renderer.Cache
 open Element
