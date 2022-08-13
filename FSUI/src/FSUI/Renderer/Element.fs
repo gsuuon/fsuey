288b19f077a2686cfca492a173ee8b78c847ce99
Steven Sun <gsuuon+dev@gmail.com>
2022-08-13 13:33:03 -0500

    Cleanup Element.create

--- FSUI/src/FSUI/Renderer/Element.fs
+++ FSUI/src/FSUI/Renderer/Element.fs
@@ -33,6 +33,24 @@ type ElementRecord<'p, 'd, 'v> =
 
 type RendersElement<'prop, 'data, 'node> = IReadOnlyCollection<'prop> -> 'data -> Position -> 'node
 
+[<AutoOpen>]
+module Util =
+    let inline gate pred fn v =
+        if pred then fn v else v
+
+    let inline gateOpt opt fn v =
+        match opt with
+        | Some x -> fn x v
+        | None -> v
+
+    let inline save (cache: Swapper<_,_,_,_>) props data pos visual =
+        try
+            cache.Fresh.Add (pos, (props, data, visual) )
+        with
+        | :? System.ArgumentException -> // TODO Is an element existing in Fresh at this position an error?
+            System.Console.Error.WriteLine ("Fresh cache already contained an element at " + pos.ToString())
+        visual
+
 let create<'prop, 'data, 'visual, 'node, 'element
                 when 'element :> IElement<'prop, 'data, 'visual>
                  and 'prop : equality
@@ -45,31 +63,13 @@ let create<'prop, 'data, 'visual, 'node, 'element
     =
     fun (props: IReadOnlyCollection<'prop>) (data: 'data) (pos: Position) ->
         let (exists, last) = cache.Stale.Remove pos // match .. with syntax doesn't get the correct overload
-        
-        let visual =
-            if exists then
-                let (props', data', visual') = last
 
-                let visual' =
-                    if data' <> data then
-                        element.Update data' data visual'
-                    else
-                        visual'
-
-                match difference props' props with
-                | Some changes ->
-                    element.Change changes visual'
-                | None ->
-                    visual'
-            else
-                element.Create props data
-
-        visual
-         |> fun el ->
-                try
-                    cache.Fresh.Add (pos, (props, data, el) )
-                with
-                | :? System.ArgumentException -> // TODO Is an element existing in Fresh at this position an error?
-                    System.Console.Error.WriteLine ("Fresh cache already contained an element at " + pos.ToString())
-                el
-         |> asNode
+        match exists, last with
+        | false, _ ->
+            element.Create props data
+        | true, (props', data', visual') ->
+            visual'
+             |> gate (data' <> data) (element.Update data' data)
+             |> gateOpt (difference props' props) element.Change
+        |>  save cache props data pos
+        |>  asNode
