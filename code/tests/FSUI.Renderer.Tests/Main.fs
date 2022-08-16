0c1b0084c533eb926185499ce170fec71119e823
Steven Sun <gsuuon+dev@gmail.com>
2022-08-15 20:30:00 -0500

    Add unquote and test utilities

--- code/tests/FSUI.Renderer.Tests/Main.fs
+++ code/tests/FSUI.Renderer.Tests/Main.fs
@@ -3,43 +3,105 @@ module FSUI.Renderer.Tests
 open Expecto
 
 open FSUI.Types
+open FSUI.Renderer.Provider
 open FSUI.Test.Provider
 open FSUI.Test.Host
 open FSUI.Elements.Views
 open FSUI.Renderer.Element
 
-let pos = Root
-let env = Env()
 
 type Model =
     { name : string
     }
 
-module TestView =
-    let view model =
-        div [] [
-            text [] "Hi"
-        ]
+let pos = Root
+let env = Env()
 
-    let model =
-        {
-            name = "foo"
+module Utils =
+    open System
+    open System.Diagnostics
+    open Swensen.Unquote.Assertions
+    open Swensen.Unquote.Operators
+
+    let chopLast (xs: 'a list) =
+        xs[..xs.Length - 2]
+
+    [<StackTraceHidden>]
+    let that msg expr =
+        try 
+            test expr
+        with
+        | _ ->
+            let reduction = // TODO colors?
+                expr
+                 |> reduceFully
+                 |> chopLast
+                 |> List.map decompile
+
+            raise <| Expecto.AssertException (msg :: reduction |> String.concat "\n")
+            
+    let mutations (x: Visual) = x.MutationLog.Length
+
+module TestView =
+    open Utils
+
+    open Expecto.Expect
+
+    [<Tests>]
+    let scratch =
+        test "basic" {
+            that "it renders build items" <@ 1 + 2 = 1 @>
+            // "render builds items" |> equal (1+2) 1
         }
 
-    let rootElement = view model env pos
 
     [<Tests>]
     let tests =
-        testList "View once" [
-            testCase "container elements correct" <| fun _ ->
-                let visualRoot = rootElement :?> VisualCollection
-                match visualRoot.Children with
-                | [el1] ->
-                    let textElement = el1 :?> VisualText
+        let view model =
+            div [] [
+                text [] "Hi"
+                text [] model.name
+            ]
 
-                    Expect.equal textElement.Content $"Hi {model.name}" "Text content"
+        let model =
+            {
+                name = "foo"
+            }
+
+        let rootElement = view model env pos
+
+        let swap (provider: #IProvider) = provider.Cache.Swap()
+        let children (v: Visual) = (v :?> Collection).Children
+
+        let failsChildren elName =
+            Tests.failtest (sprintf "%s contains wrong number or type of children" elName)
+
+        testList "Basic view" [
+            // FIXME tests should generally be independent or in a testSequenced
+            test "first render" {
+                match children rootElement with
+                | [ :? Text as text1 
+                    :? Text as text2
+                  ] ->
+                    "no mutations in text1" |> equal (mutations text1) 0
+                    "no mutations in text2" |> equal (mutations text2) 0
+
                 | _ ->
-                    Tests.failtest "Root contains wrong number of children"
+                    failsChildren "Root"
+            }
+
+            test "second render, no changes" {
+                swap env
+                match view model env pos |> children with
+                | [ :? Text as text1
+                    :? Text as text2
+                  ] ->
+                    "no mutations in text1" |> equal (mutations text1) 0
+                    "no mutations in text2" |> equal (mutations text2) 0
+                | _ ->
+                    failsChildren "Root"
+
+            }
         ]
 
 module TestRenderer =
