0e7661ce93f7ebaf76661f8cd17b5f9fbf2ed8d8
Steven Sun <gsuuon+dev@gmail.com>
2022-08-18 04:58:07 -0500

    Dedupe show and that

--- code/src/utilsTest.fsx
+++ code/src/utilsTest.fsx
@@ -8,30 +8,34 @@ module private State =
     let mutable testCount = 0
     let mutable hasErrored = false
 
+let private errored msg (e: exn) =
+    State.hasErrored <- true
+
+    let lines = e.Message.Split '\n'
+    let last = lines.Length - 2 // last is empty after \n
+
+    lines
+     |> Array.iteri
+        ( fun idx line ->
+            if idx = 0 then
+                eprintf "Fails that: "
+                Console.ForegroundColor <- ConsoleColor.Red
+                eprintfn "%s" msg
+                Console.ResetColor()
+            elif idx = last then
+                if not <| line.StartsWith "false" then
+                    eprintfn "%s" line
+            else
+                eprintfn "%s" line
+        )
+    
 let that msg expr =
     try
         test expr
         State.testCount <- State.testCount + 1
     with
     | :? Swensen.Unquote.AssertionFailedException as e ->
-        State.hasErrored <- true
-        let lines = e.Message.Split '\n'
-        let last = lines.Length - 2 // last is empty after \n
-
-        lines
-         |> Array.iteri
-            ( fun idx line ->
-                if idx = 0 then
-                    eprintf "Fails that: "
-                    Console.ForegroundColor <- ConsoleColor.Red
-                    eprintfn "%s" msg
-                    Console.ResetColor()
-                elif idx = last then
-                    if not <| line.StartsWith "false" then
-                        eprintfn "%s" line
-                else
-                    eprintfn "%s" line
-            )
+        errored msg e
 
 let show msg expr =
     try
@@ -49,24 +53,7 @@ let show msg expr =
         printfn "%s" reduction
     with
     | :? Swensen.Unquote.AssertionFailedException as e ->
-        State.hasErrored <- true
-        let lines = e.Message.Split '\n'
-        let last = lines.Length - 2 // last is empty after \n
-
-        lines
-         |> Array.iteri
-            ( fun idx line ->
-                if idx = 0 then
-                    eprintf "Fails that: "
-                    Console.ForegroundColor <- ConsoleColor.Red
-                    eprintfn "%s" msg
-                    Console.ResetColor()
-                elif idx = last then
-                    if not <| line.StartsWith "false" then
-                        eprintfn "%s" line
-                else
-                    eprintfn "%s" line
-            )
+        errored msg e
 
 let exitCode () =
     if State.hasErrored then
