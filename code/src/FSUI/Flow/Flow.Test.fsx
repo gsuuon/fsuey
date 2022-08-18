c6e35ac002fbb1e4600f1da2011d2a23071a3bda
Steven Sun <gsuuon+dev@gmail.com>
2022-08-18 01:33:16 -0500

    Add Flow unit tests

--- code/src/FSUI/Flow/Flow.Test.fsx
+++ code/src/FSUI/Flow/Flow.Test.fsx
@@ -1,62 +1,51 @@
-#load "Flow.fs"
-// TODO write tests
+#load "../TestReferences.fsx"
+open TestReferences
 
+#load "Flow.fs"
 open FSUI.Flow
 
-type MyWaitType = MyWaitType of string
-type MyReturnType = MyReturnType of string
+type Waits = Waits of string
+type Returns = Returns of string
 
-let foo : Flow<unit, MyWaitType> = 
-    flow {
-        let mutable x = 0
-        printfn "\tfoo: start"
-        yield (MyWaitType "wait one")
-        while x < 3 do
-            printfn "doing %i" x
-            x <- x + 1
-            yield (MyWaitType (sprintf "wait while %i" x))
+open System.Collections
 
-        while x < 2 do
-            printfn "doing %i" x
-            x <- x + 1
-            yield (MyWaitType (sprintf "wait while %i" x))
-        printfn "\tfoo: end"
-    }
+let toList x = x |> Flow.asSeq |> Seq.toList
 
-let enumerate name (eventual: Flow<_,_>) =
-    let mutable steps = 0
-    printfn "\n\n----- Enumerating %s\n%A\n-----" name eventual
+that "wait values are enumerated"
+    <@ flow
+        { yield Waits "foo"
+          yield Waits "bar" } |> toList
+        =
+        [ Waits "foo"
+          Waits "bar" ]
+    @>
 
-    for x in eventual do
-        printfn "%i> %A" steps x
-        steps <- steps + 1
+that "return values are also enumerated"
+    <@ flow
+        { yield Waits "foo"
+          return Returns "bar" } |> toList
+        =
+        [ Waits "foo"   :> obj
+          Returns "bar" :> obj ]
+    @>
 
-    printfn "Took %i steps\n-----" steps
+that "nested flows are enumerated, return value is bound"
+    <@ flow
+        { yield Waits "foo"
+          let! x =
+            flow {
+                yield Waits "bar"
+                return Returns "quix"
+            }
+          yield Waits "baz"
+          return x
+        } |> toList
+        =
+        [ Waits "foo"    :> obj
+          null           // TODO try to get rid of this null
+          Waits "bar"    :> obj
+          Waits "baz"    :> obj
+          Returns "quix" :> obj ]
+    @>
 
-enumerate (nameof foo) foo
-
-(*
-
-:!dotnet fsi .\Flow\Flow.Test.fsx
-
-
-
------ Enumerating foo
-Wait (None, Some <fun:foo@10>)
------
-0> None
-	foo: start
-1> None
-doing 0
-2> None
-doing 1
-3> None
-doing 2
-4> None
-5> None
-6> None
-	foo: end
-7> Some ()
-Took 8 steps
------
-*)
+exitCode()
