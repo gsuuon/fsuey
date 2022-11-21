84a6563b42db65ae0e2af6bef130dba5a3b850d6
Steven Sun <steven@toyworlds.co>
2022-11-21 13:15:28 -0500

    Add view element overloads, remove does

--- code/src/FSUI/Elements/Views.fs
+++ code/src/FSUI/Elements/Views.fs
@@ -3,19 +3,11 @@ module FSUI.Elements.Views
 open FSUI.Types
 open FSUI.Elements.Interfaces
 
-// This applies 'env
-let inline mkRender<'prop, 'data, 'node, 'env>
-    ([<InlineIfLambda>]
-        renderer: 'env -> Applies<'prop, 'data, 'node>)
-    (props: 'prop collection)
-    (data: 'data)
-    (env: 'env)
-    (pos: Position)
-    =
-    (renderer env) props data pos
+let gameObject props data (env: #IGameObject<_,_,_>) pos =
+    env.GameObject props data pos
 
-let text x = mkRender (fun (env: #IText<'prop, 'node>) -> env.Text) x
-let gameObject x = mkRender (fun (env: #IGameObject<_,_,_>) -> env.GameObject) x
+let text props data (env: #IText<'prop, 'node>) pos =
+    env.Text props (string data) pos
 
 let div props children (env: #IContainer<_,_>) pos =
     let nodes : 'node list =
@@ -35,9 +27,26 @@ let join props children (env: #IJoinContain<_,_,_>) pos =
 
     env.JoinContain props nodes pos
 
+/// Stringly key an element to preserve it against reordering
 let at (name: string) (renderer: 'env -> Position -> 'visual) =
-    fun env (pos: Position) ->
-        renderer env (pos.Named name)
+    fun env (pos: Position) -> renderer env (pos.Named name)
 
-/// Better semantics for Button
-let does (k, v) = Keyed (k, v)
+type Elements<'prop> =
+    // We need to annotate the return types for many of these overloads to ensure they're sufficiently generic
+
+    static member inline text (props: 'prop list) : 'a -> 'b -> Position -> 'c =
+        fun a -> text props a
+    static member inline text a : 'a -> Position -> 'b =
+        text [] a
+
+    static member inline button x =
+        fun action -> button [] ( text [] x, Keyed (x, action ) )
+    static member inline button (x, key) =
+        fun action -> button [] ( text [] x, Keyed (key, action ) )
+    static member inline button (x: Renders<_,_>, key) =
+        fun action -> button [] ( x, Keyed (key, action ) )
+
+    static member inline div (children: Renders<_,_> list) =
+        div [] children
+    static member inline div (props: 'prop list) =
+        fun (children: Renders<_,_> list) -> div props children
