b9808f14b98c213a7a5a40ee9e33960ba25f2994
Steven Sun <steven@toyworlds.co>
2022-11-22 12:34:15 -0500

    Add mkStoreByIngest with example

--- code/src/FSUI/Make/LayoutStateView.fs
+++ code/src/FSUI/Make/LayoutStateView.fs
@@ -41,6 +41,94 @@ type Component<'layout, 'state, 'msg, 'node>(
     interface Runner<'layout,'state,'msg,'node> with
         member _.View = view
         member _.Render () = this.doRender ()
-    
+
+type ShouldUpdate<'state> =
+    | Update of 'state
+    | NoUpdate
+
+let mkStoreByIngest ingest initialState render =
+    let mutable state = initialState
+
+    let set updater =
+        match updater state with
+        | Update state' ->
+            state <- state'
+            render()
+        | NoUpdate ->
+            ()
+
+    { new Store<_,_> with
+        member _.State = state
+        member this.Dispatch msg = ingest msg set
+    }
+
 let make initialLayout mkStore show render =
     Component(initialLayout, mkStore, show, render) :> Runner<_,_,_,_>
+
+module App =
+    module Domain =
+        type Msg =
+            | Increment
+            | Decrement
+
+        type State =
+            { x : int }
+
+    open Domain
+
+    let text _ = ()
+    let button _ = ()
+
+    type Layout =
+        | A
+        | B
+        | C
+
+    let showMain (vm: View<_,_,_>) =
+        function
+        | A -> text vm.State.x
+        | B -> button (fun _ -> vm.Dispatch Increment)
+        | C -> button (fun _ -> vm.Layout B)
+
+    let myStore init render =
+        let mutable state = init
+
+        async {
+            while true do
+                do! Async.Sleep 1000
+                state <- { state with x = state.x + 1 }
+                render ()
+        } |> Async.Start
+
+        { new Store<_,_> with
+            member _.State = state
+            member this.Dispatch msg = 
+                match msg with
+                | Increment ->
+                    async {
+                        if this.State.x < 10 then
+                            state <- { state with x = this.State.x + 1 }
+                            render ()
+                        else
+                            ()
+                    } |> Async.Start
+                | Decrement ->
+                    ()
+        }
+
+    let myIngestStore =
+        mkStoreByIngest
+         <| fun msg update ->
+                match msg with
+                | Increment ->
+                    async {
+                        update
+                         <| function
+                            | { x = x } when x < 10 ->
+                                Update { x = x + 1 }
+                            | _ ->
+                                NoUpdate
+                    } |> Async.Start
+                | Decrement ->
+                    ()
+    let runner renderer = make A (myStore { x = 0 }) showMain renderer
