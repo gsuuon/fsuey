e3b8abd20613a270a4eb3125e7aa581366138ef0
Steven Sun <steven@toyworlds.co>
2022-11-22 12:24:52 -0500

    Treat cyclic references as component

--- code/src/FSUI/Make/LayoutStateView.fs
+++ code/src/FSUI/Make/LayoutStateView.fs
@@ -16,10 +16,17 @@ type Runner<'layout, 'state, 'msg, 'node> =
     abstract Render : unit -> unit
 
 
-type Program<'layout, 'state, 'msg, 'node>(initialLayout, mkStore, show, render) as this =
+type Component<'layout, 'state, 'msg, 'node>(
+    // Class to contain the recursive references between view store and render
+        initialLayout : 'layout,
+        mkStore : (unit -> unit) -> Store<'state, 'msg>,
+        show          : View<_,_,_> -> 'layout -> 'node,
+        render        : 'node -> unit
+    ) as this
+    =
+
     let mutable layout = initialLayout
     let store : Store<'state, 'msg> = mkStore this.doRender
-
     let view =
         { new View<_,_,_> with
             member _.Layout layout' =
@@ -29,60 +36,11 @@ type Program<'layout, 'state, 'msg, 'node>(initialLayout, mkStore, show, render)
             member _.State = store.State
         }
 
-    member _.doRender () =
-        show view store layout |> render
+    member _.doRender () = show view layout |> render
 
     interface Runner<'layout,'state,'msg,'node> with
         member _.View = view
         member _.Render () = this.doRender ()
     
-// let make
-//     (initialLayout : 'layout)
-//     (mkStore : (unit -> unit) -> Store<'state, 'msg>)
-//     (show          : View<_,_,_> -> Store<'state, 'msg> -> 'layout -> 'node)
-//     (render        : 'node -> unit)
-//                    : Runner<'layout, 'state, 'msg, 'node>
-//                    =
-//         let mutable layout = initialLayout
-
-//         let rec view = 
-//             { new View<_,_,_> with
-//                 member _.Layout layout' =
-//                     layout <- layout'
-//                     doRender ()
-//             }
-
-//         and store = mkStore doRender
-//         and doRender () =
-//             show view store layout |> render
-
-//         { new Runner<'layout, 'state, 'msg, 'node> with
-//             member _.View = view
-//             member _.Render () = doRender ()
-//         }
-
-module App =
-    module Domain =
-        type Msg =
-            | Increment
-            | Decrement
-
-        type State =
-            { x : int
-            }
-
-    open Domain
-
-    let text _ = ()
-    let button _ = ()
-
-    type Layout =
-        | A
-        | B
-        | C
-
-    let showMain (vm: View<_,_,_>) =
-        function
-        | A -> text vm.State.x
-        | B -> button (fun _ -> vm.Dispatch Increment)
-        | C -> button (fun _ -> vm.Layout B)
+let make initialLayout mkStore show render =
+    Component(initialLayout, mkStore, show, render) :> Runner<_,_,_,_>
