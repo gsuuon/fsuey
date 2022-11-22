d1c80d94b4e01c284edbdd5766452d0619dce894
Steven Sun <steven@toyworlds.co>
2022-11-22 12:06:04 -0500

    As a class

--- code/src/FSUI/Make/LayoutStateView.fs
+++ code/src/FSUI/Make/LayoutStateView.fs
@@ -7,45 +7,59 @@ type Store<'state, 'msg> =
     abstract State : 'state
 
 type View<'layout, 'state, 'msg> =
+    abstract Layout : 'layout -> unit
     abstract Dispatch : 'msg -> unit
     abstract State : 'state
-    abstract Layout : 'layout -> unit
 
 type Runner<'layout, 'state, 'msg, 'node> =
     abstract View : View<'layout, 'state, 'msg>
     abstract Render : unit -> unit
 
-let make
-    (initialLayout : 'layout)
-    (initialState : 'state)
-    (ingest : 'msg -> 'state -> ( ('state -> 'state) -> unit) -> unit)
-    (show          : View<'layout, 'state, 'msg> -> 'layout -> 'node)
-    (render        : 'node -> unit)
-                   : Runner<'layout, 'state, 'msg, 'node>
-                   =
-        let mutable layout = initialLayout
-        let mutable state = initialState
 
-        let rec view = 
-            { new View<'layout, 'state, 'msg> with
-                member _.Dispatch msg = ingest msg state set
-                member _.State = state
-                member _.Layout layout' =
-                    layout <- layout'
-                    doRender ()
-            }
+type Program<'layout, 'state, 'msg, 'node>(initialLayout, mkStore, show, render) as this =
+    let mutable layout = initialLayout
+    let store : Store<'state, 'msg> = mkStore this.doRender
 
-        and set update =
-            state <- update state
-            doRender()
-
-        and doRender () =
-            show view layout |> render
-
-        { new Runner<'layout, 'state, 'msg, 'node> with
-            member _.View = view
-            member _.Render () = doRender ()
+    let view =
+        { new View<_,_,_> with
+            member _.Layout layout' =
+                layout <- layout'
+                this.doRender ()
+            member _.Dispatch x = store.Dispatch x
+            member _.State = store.State
         }
+
+    member _.doRender () =
+        show view store layout |> render
+
+    interface Runner<'layout,'state,'msg,'node> with
+        member _.View = view
+        member _.Render () = this.doRender ()
+    
+// let make
+//     (initialLayout : 'layout)
+//     (mkStore : (unit -> unit) -> Store<'state, 'msg>)
+//     (show          : View<_,_,_> -> Store<'state, 'msg> -> 'layout -> 'node)
+//     (render        : 'node -> unit)
+//                    : Runner<'layout, 'state, 'msg, 'node>
+//                    =
+//         let mutable layout = initialLayout
+
+//         let rec view = 
+//             { new View<_,_,_> with
+//                 member _.Layout layout' =
+//                     layout <- layout'
+//                     doRender ()
+//             }
+
+//         and store = mkStore doRender
+//         and doRender () =
+//             show view store layout |> render
+
+//         { new Runner<'layout, 'state, 'msg, 'node> with
+//             member _.View = view
+//             member _.Render () = doRender ()
+//         }
 
 module App =
     module Domain =
