0a41524af32ed980d0749cda3d2226cbae3999a3
Steven Sun <steven@toyworlds.co>
2022-11-22 19:56:42 -0500

    recreate the button

--- code/src/FSUI.Renderer.Unity/Provider.fs
+++ code/src/FSUI.Renderer.Unity/Provider.fs
@@ -166,13 +166,28 @@ type UnityProvider() =
                     ScreenNode.addChild
                         (Button (System.Action action) ) // directly stick the fn on without converting to delegate or we can't remove
                         child
-                update = fun (_, Keyed (_, action') ) (child, Keyed (_, action) ) e ->
-                    e.remove_clicked action'
-                    e.add_clicked action
 
-                    ScreenNode.addChild e child
+                update = fun (_, Keyed (_, action') ) (child, Keyed (_, action) ) e ->
+                    // FIXME
+                    // There's no way to just remove all the event handlers from button from F# and I can't
+                    // get removing handlers to work even wrapping before here
+
+                    // e.remove_clicked action'
+                        // Trying to remove the previous action doesn't seem to work, I'm not sure why
+                        // should be the same Action instance since we're pulling from the cache for the previous
+                    // e.add_clicked action
+
+                    // ScreenNode.addChild e child
+
+                    // Just destroy and recreate
+                    Graph.remove e
+
+                    ScreenNode.addChild
+                        (Button action)
+                        child
             }
 
+    // TODO remove
     interface IButton<ScreenProp, ScreenElement * (unit -> unit), ScreenElement> with
         member _.Button = noKeyButton
     
