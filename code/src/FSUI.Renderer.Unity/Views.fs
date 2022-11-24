f9e0fef91222c4eaf51bab59010ca145da60b0a8
Steven Sun <steven@toyworlds.co>
2022-11-24 16:40:34 -0500

    Add prefab overloads

--- code/src/FSUI.Renderer.Unity/Views.fs
+++ code/src/FSUI.Renderer.Unity/Views.fs
@@ -3,10 +3,15 @@ module FSUI.Renderer.Unity.Views
 open FSUI.Types
 open FSUI.Renderer.Unity
 
-let prefab path name props children (env: UnityProvider) pos =
-    let nodes =
-        children |> List.mapi (fun idx fnode -> fnode env (Ordinal (pos, idx)))
+type GameElement =
+    static member prefab (path, name) =
+        fun props children (env: UnityProvider) pos ->
+            let nodes =
+                children |> List.mapi (fun idx fnode -> fnode env (Ordinal (pos, idx)))
 
-    let cacheName = path + ":" + name
+            let cacheName = path + ":" + name
 
-    env.Prefab path name props nodes (pos.Named cacheName)
+            env.Prefab path name props nodes (pos.Named cacheName)
+
+    static member prefab (name) =
+        GameElement.prefab(name, name)
